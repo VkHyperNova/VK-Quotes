af4b3316b06ed33d29e0fbfc3cdc1f85493ec4af
Veikko <kiis.veikko@gmail.com>
2024-12-17 16:14:38 +0200

    Commandline optimizations

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 	"vk-quotes/pkg/config"
 	"vk-quotes/pkg/db"
 	"vk-quotes/pkg/util"
@@ -12,14 +13,16 @@ func CommandLine(quotes *db.Quotes) {
 
 	quotes.PrintCLI()
 
-	var input string = ""
-	var inputID int = 0
-
-	fmt.Print("=> ")
-
-	fmt.Scanln(&input, &inputID)
-
 	for {
+		var input string = ""
+		var inputID int = 0
+
+		fmt.Print("=> ")
+
+		fmt.Scanln(&input, &inputID)
+
+		input = strings.ToLower(input)
+
 		switch input {
 
 		case "add", "a":
@@ -27,51 +30,38 @@ func CommandLine(quotes *db.Quotes) {
 			if quotes.UserInput(emptyQuote) {
 				quotes.Add()
 			}
-			CommandLine(quotes)
 		case "update", "u":
 			UpdateQuote := quotes.FindQuoteByID(inputID)
 			if quotes.UserInput(UpdateQuote) {
 				quotes.Update(inputID)
 			}
-			CommandLine(quotes)
 
 		case "delete", "d":
 			quotes.Delete(inputID)
-			CommandLine(quotes)
 
 		case "find", "f":
 			quotes.Find()
-			CommandLine(quotes)
 
 		case "showall", "s":
 			quotes.PrintAllQuotes()
-			CommandLine(quotes)
 
 		case "stats":
 			quotes.PrintStatistics()
-			CommandLine(quotes)
 
 		case "resetids":
 			quotes.ResetIDs(quotes)
-			CommandLine(quotes)
 
 		case "read", "r":
 			quotes.Read()
-			CommandLine(quotes)
-
 		case "similarquotes", "sim":
 			db.FindSimilarQuotes(quotes)
-			CommandLine(quotes)
-
 		case "q", "quit":
-			// Backup
 			quotes.Backup()
 			util.ClearScreen()
 			os.Exit(0)
 
 		default:
 			config.AddMessage("Enter pressed!")
-			CommandLine(quotes)
 		}
 	}
 }
